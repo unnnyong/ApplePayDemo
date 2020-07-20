62f59a8a58f48e23f026cc4b68465cbeecf54b16
unnnyong <unnnyong@gmail.com>
2020-07-20 23:59:50 +0900

    add Apple pay codes

--- ApplePayDemo/ApplePayDemo/ViewControllers/ViewController.swift
+++ ApplePayDemo/ApplePayDemo/ViewControllers/ViewController.swift
@@ -23,10 +23,27 @@ final class ViewController: UIViewController {
 
 }
 
+// MARK: PKPaymentAuthorizationViewControllerDelegate
+extension ViewController: PKPaymentAuthorizationViewControllerDelegate {
+
+    func paymentAuthorizationViewControllerDidFinish(_ controller: PKPaymentAuthorizationViewController) {
+        paymentStatusLabel.isHidden = false
+    }
+
+    func paymentAuthorizationViewController(
+        _ controller: PKPaymentAuthorizationViewController,
+        didAuthorizePayment payment: PKPayment,
+        handler completion: @escaping (PKPaymentAuthorizationResult) -> Void
+    ) {
+        applePayButton.isHidden = true
+        paymentStatusLabel.isHidden = false
+
+        controller.dismiss(animated: true)
+    }
+}
+
 // MARK: Private methods
 private extension ViewController {
-
-    @objc func presentToPaymentVC() {}
 
     func setupApplePayButton() {
         view.addSubview(applePayButton)
@@ -47,6 +64,49 @@ private extension ViewController {
         }
 
         applePayButton.addTarget(self, action: #selector(presentToPaymentVC), for: .touchUpInside)
+    }
+
+    @objc func presentToPaymentVC() {
+        let item = PKPaymentSummaryItem(label: "삼겹살 500g", amount: NSDecimalNumber(integerLiteral: 10_000))
+        let shippingMethod = PKShippingMethod(label: "미사일배송", amount: NSDecimalNumber(integerLiteral: 2_500))
+        let summary = PKPaymentSummaryItem(label: "전체 금액", amount: NSDecimalNumber(integerLiteral: 12_500))
+
+        let request = PKPaymentRequest()
+        request.currencyCode = "KRW"
+        request.countryCode = "JP" // "KR"은 아직 안됩니다 🥺
+        request.supportedNetworks = [.amex, .visa, .masterCard]
+        request.merchantCapabilities = .capability3DS
+        request.merchantIdentifier = "merchant.com.example.applePay" // 임의의 Identifier value
+        request.paymentSummaryItems = [item, shippingMethod, summary]
+
+        request.shippingType = .shipping
+        request.requiredShippingContactFields = [.postalAddress, .name, .phoneNumber, .emailAddress]
+
+        // 회원의 경우에만 회원정보를 설정하면 된다.
+        request.shippingContact = makeContact()
+
+        guard let paymentViewController = PKPaymentAuthorizationViewController(paymentRequest: request) else { return }
+        paymentViewController.delegate = self
+
+        present(paymentViewController, animated: true)
+    }
+
+    func makeContact() -> PKContact {
+        let userInfo = UserInfo.shared
+        let contact = PKContact()
+
+        contact.emailAddress = userInfo.email
+        contact.phoneNumber = CNPhoneNumber(stringValue: userInfo.phoneNumber)
+
+        var name = PersonNameComponents()
+        name.givenName = userInfo.name
+        contact.name = name
+
+        let address = CNMutablePostalAddress()
+        address.street = userInfo.address1
+        contact.postalAddress = address
+
+        return contact
     }
 
 }
