f52a07857f377d8c1d61e9561973f5e7dd75a8e7
unnnyong <unnnyong@gmail.com>
2020-07-20 23:54:06 +0900

    add ApplePayButton on ViewController

--- ApplePayDemo/ApplePayDemo/ViewControllers/ViewController.swift
+++ ApplePayDemo/ApplePayDemo/ViewControllers/ViewController.swift
@@ -7,9 +7,46 @@
 //
 
 import UIKit
+import PassKit
 
 final class ViewController: UIViewController {
 
     @IBOutlet private weak var paymentStatusLabel: UILabel!
+
+    private let applePayButton = PKPaymentButton(paymentButtonType: .buy, paymentButtonStyle: .black)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupApplePayButton()
+    }
+
+}
+
+// MARK: Private methods
+private extension ViewController {
+
+    @objc func presentToPaymentVC() {}
+
+    func setupApplePayButton() {
+        view.addSubview(applePayButton)
+
+        applePayButton.translatesAutoresizingMaskIntoConstraints = false
+
+        [
+            applePayButton.centerXAnchor.constraint(
+                equalToSystemSpacingAfter: view.centerXAnchor,
+                multiplier: 0.0
+            ),
+            applePayButton.centerYAnchor.constraint(
+                equalToSystemSpacingBelow: view.centerYAnchor,
+                multiplier: 0.0
+            )
+        ].forEach { constraint in
+            constraint.isActive = true
+        }
+
+        applePayButton.addTarget(self, action: #selector(presentToPaymentVC), for: .touchUpInside)
+    }
 
 }
