0c6f7226000d14aaccb27c8487cc92644ea84290
unnnyong <unnnyong@gmail.com>
2020-07-20 23:46:50 +0900

    add new Xcode project file

---
+++ ApplePayDemo/ApplePayDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ApplePayDemo
+//
+//  Created by Eunyeong Kim on 2020/07/20.
+//  Copyright © 2020 Eunyeong Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
